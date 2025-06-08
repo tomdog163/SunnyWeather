87f789d8eb1dfd8fd48f6293df470798787c290b
tomdog163 <tomdog163@163.com>
2025-06-08 17:05:34 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -9,6 +9,14 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 object SunnyWeatherNetwork {
+    private val weatherService = ServiceCreator.create(WeatherService::class.java)
+
+    suspend fun getDailyWeather(lng: String, lat: String) =
+        weatherService.getDailyWeather(lng, lat).await()
+
+    suspend fun getRealtimeWeather(lng: String, lat: String) =
+        weatherService.getRealtimeWeather(lng, lat).await()
+
     // 创建PlaceService接口的动态代理对象
     private val placeService = ServiceCreator.create<PlaceService>()
 
